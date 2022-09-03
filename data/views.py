823334007054828b099bfdd923ba5291e45f67ee
zednq <110108611+zednq@users.noreply.github.com>
2022-09-03 19:06:22 +0200

    Update views.py

--- data/views.py
+++ data/views.py
@@ -5,6 +5,25 @@ def redirectview(request):
     return redirect('/stevner/page=1&show=10')
 
 def table(request, page=1, show=25, search='', order='lofter'):
+    
+    sort_dict = {
+        "idx":'index',
+        "lofter":'navn',
+        "aar":'stevnedato (tidlig)',
+        "-aar":'stevnedato (sent)',
+        "fodt":'fodselsaar (tidlig)',
+        "-fodt":'fodselsaar (sent)',
+        "-rykk":'rykk (mest)',
+        "rykk":'rykk (minst)',
+        "-stot":'stot (mest)',
+        "stot":'stot (minst)',
+        "-sml":'sammenlagt (mest)',
+        "sml":'sammenlagt (minst)',
+        "-sinclair":'sinclair (mest)',
+        "sinclair":'sinclair (minst)',
+    }
+    sort_string = sort_dict[order]
+    
     path = ''
     def get_data_range(page, show, search, order):
         entries = show
@@ -32,6 +51,7 @@ def table(request, page=1, show=25, search='', order='lofter'):
         return redirect('/stevner/page=1&show=10')
 
     context = {
+        'sort_string':sort_string,
         'search_string': search,
         'order': order_path,
         'search': path,
