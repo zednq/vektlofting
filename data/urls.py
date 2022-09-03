f747189b6f42e6dbe1f71d18490b4bd70086bbd7
zednq <110108611+zednq@users.noreply.github.com>
2022-09-03 14:39:35 +0200

    Update urls.py

--- data/urls.py
+++ data/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.redirectview),
+    path('', views.table),
     path('stevner/page=<int:page>&show=<int:show>', views.table, name='index'),
     path('stevner/page=<int:page>&show=<int:show>/search=<search>', views.table, name='search'),
     path('stevner/page=<int:page>&show=<int:show>/order_by=<order>', views.table, name='search'),
